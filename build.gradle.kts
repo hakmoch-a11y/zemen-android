55e1c46f5a1a6042f722b3f6f8243c33e1a87abf
king <king@kali.kali>
2026-09-14 13:06:03 +0300

    Fix SDK module version alignment for v1.0.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,12 @@
 group = "com.github.hakmoch-a11y"
-version = providers.gradleProperty("VERSION_NAME").orElse("1.0.2").get()
+version = providers.gradleProperty("VERSION_NAME").orElse("1.0.3").get()
 
 plugins {
     id("com.android.library") version "8.5.2" apply false
     kotlin("android") version "1.9.25" apply false
     kotlin("jvm") version "1.9.25" apply false
+}
+
+subprojects {
+    version = rootProject.version
 }
